f3d6c9bfd67c28f723e85f89d0ddcb3fce8b0e39
dhkang <alceste951005@naver.com>
2023-12-20 17:02:53 +0900

    Feat : add userDomain,repository,userService and RepositoryTestCase

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,25 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void 회원가입() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
