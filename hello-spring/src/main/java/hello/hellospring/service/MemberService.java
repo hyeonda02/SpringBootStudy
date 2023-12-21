c161a7885cfd6a75235c96c4f346b40274e94086
dhkang <alceste951005@naver.com>
2023-12-21 21:18:09 +0900

    Feat : JPA

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-//@Service
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     //스프링 컨테이너에 등록할때 생성자를 자동으로 실행하고, 스프링 컨테이너에 있는 memberRepository듀 등록한다
