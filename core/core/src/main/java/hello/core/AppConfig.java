2b72428350d89d8e0de0308d448f8421177198cd
dhkang <alceste951005@naver.com>
2023-12-29 00:12:12 +0900

    Feat : getBean

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -10,21 +10,32 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 //중복 제거 & 역활에 따라 기능을 구현
+//Config : 설정 정보 Bean :각 메소드에 붙임 ( 스프링 컨테이너에 등록됨 )
+@Configuration
 public class AppConfig {
+
+    @Bean(name="mmm")
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    @Bean
     public OrderService orderService(){
         //return new OrderServiceImpl(memberRepository(), discountPolicy());
         return new OrderServiceImpl(memberRepository(), rateDiscountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
+    @Bean
     public DiscountPolicy rateDiscountPolicy(){
         return new RateDiscountPolicy();
     }
